d1547494e1a379ed0159a52d32ae62190bf3556a
Jowi <xoanmanuelaltaleffpais@gmail.com>
2022-09-07 19:41:25 -0300

    poronga

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,39 @@
 import styled from "styled-components";
 // Jowi
+import {HashLink as Link } from "react-router-hash-link" 
 const StyledHeader= styled.section`
-    /*  Here you can write css. The css will cascade to all the children. 
-        https://styled-components.com/docs
-    */
-    color: red;
+   img#rss {
+
+    }
+
+    img#rss { 
+        display: flex;
+        flex-direction: row-reverse;
+        height: 50px;
+        width: 50px;
+        margin-left: 3px;
+        margin-right: 3px;
+
+    }
+    color: black;
 `;
 
 export default function Header(params) {
     return (
         <StyledHeader>
-            Here you can write html
-            https://reactjs.org/docs/hello-world.html
+            <h1>Mi solcito</h1>
+            <h2>Sabores que te llevan a las estrellas</h2>
+            <img src="tortasaborpija.png" alt="tortasaborpija"/>
+
+            <div id="rrss">
+            <a href="https://www.facebook.com" target="_blank"><img src="facebooklogo.png" alt="facebook" id="rss" /></a>
+            
+            <a href="https://www.instagram.com" target="_blank"><img src="iglogo.png" alt="instagram" id="rss" /></a>
+            
+            <a href="https://www.pornhub.com" target="_blank"><img src="twitterlogo.png" alt="twitter" id="rss" /></a>
+            
+            
+            </div>
             {/* And here you can write javascript. Remember to enclose it in curly braces. 
                 https://developer.mozilla.org/en-US/docs/Web/JavaScript
             */}
