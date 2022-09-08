1adcb0a262f40ffd6b448646d80c35f85fdc1ca6
Ariel <arielghiaf@gmail.com>
2022-09-07 21:51:20 -0300

    pija

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,19 +1,73 @@
 import styled from "styled-components";
 // Ghia
 const StyledFooter= styled.section`
-    /*  Here you can write css. The css will cascade to all the children. 
-        https://styled-components.com/docs
-    */
-    color: red;
+    .main-footer {
+        
+         display: flex;
+         flex-direction: row;
+         color: white;
+         background-color: grey;
+         padding-top: 3em;
+         width: 100%;
+}       
+
+    ul{
+
+        list-style: none;
+        
+        
+
+    }
+    .col2{
+        
+       
+
+    }
+    .col3{
+        
+
+    }
+
 `;
 export default function Footer(params) {
     return (
         <StyledFooter>
-            Here you can write html
-            https://reactjs.org/docs/hello-world.html
-            {/* And here you can write javascript. Remember to enclose it in curly braces. 
-                https://developer.mozilla.org/en-US/docs/Web/JavaScript
-            */}
+           <div className="main-footer">
+          {/* Column1 */}
+          <div className="col1">
+            <h4>Menu</h4>
+                <ul>
+              <li>Inicio</li>
+              <li>Productos</li>
+              <li>Nosotros</li>
+              <li>Contáctanos</li>
+              </ul>
+          </div>
+          {/* Column2 */}
+          <div className="col2">
+            <h4>Contactanos</h4>
+            <ul className="list-unstyled">
+              <li>Direccion del local</li>
+              <li>Tel: 4596-9094</li>
+              <li>Nuestras redes sociales</li>
+              <li>Face instanashe tuiter</li>
+            </ul>
+          </div>
+          {/* Column3 */}
+          <div className="col3">
+            <h4>Ayuda</h4>
+            <ul className="list-unstyled">
+              <li>Preguntas Frecuentes</li>
+              <li>Políticas De Privacidad</li>
+              <li>Términos y Condiciones Legales</li>
+              <li>C Copyright 2022</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          
+        </div>
         </StyledFooter>
     )
 }
