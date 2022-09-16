bb18c1841664ba4d879adbef17e0cf80d1ae3363
Jowi <xoanmanuelaltaleffpais@gmail.com>
2022-09-16 18:41:56 -0300

    termina header

--- client/src/components/homepage/Header.js
+++ client/src/components/homepage/Header.js
@@ -2,20 +2,44 @@ import styled from "styled-components";
 import Navbar from "../Navbar";
 // Jowi
 const StyledHeader= styled.header`
-   img#rss {
+div.rss {
+    display: flex;
+    flex-direction: row-reverse;    
 
-    }
+}
 
-    img#rss { 
-        display: flex;
-        flex-direction: row-reverse;
+
+div.rss img{
         height: 50px;
         width: 50px;
-        margin-left: 3px;
-        margin-right: 3px;
-
+        margin: 20px;
     }
-    color: black;
+    
+div.inicio {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+}
+
+
+div span{
+    font-family: Arial, Helvetica, Sans-serif; 
+    font-size: 30px;
+    flex-direction: column;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+
+}
+
+div span h2{
+    font-family: Arial, Helvetica, Sans-serif; 
+    font-size: 27px;
+    flex-direction: column;
+    text-align: center;
+
+}
 `;
 
 export default function Header(params) {
@@ -23,13 +47,20 @@ export default function Header(params) {
         <StyledHeader>
             <Navbar />
             <div className="formatCenter">
+
+                <div className="inicio">
+            
+                <span>
                 <h1>Mi solcito</h1>
                 <h2>Sabores que te llevan a las estrellas</h2>
-                <img src="tortasaborpija.png" alt="tortasaborpija"/>
-                <div id="rrss">
-                    <a href="https://www.facebook.com" target="_blank" rel="noreferrer"><img src="facebooklogo.png" alt="facebook" id="rss" /></a>
-                    <a href="https://www.instagram.com" target="_blank" rel="noreferrer"><img src="iglogo.png" alt="instagram" id="rss" /></a>
-                    <a href="https://www.pornhub.com" target="_blank" rel="noreferrer"><img src="twitterlogo.png" alt="twitter" id="rss" /></a>
+                </span>
+                <img src="tortasaborpija.png" alt="tortasaborpija" id="tortapija"/>
+                </div>
+            
+                <div className="rss">
+                    <a href="https://www.facebook.com" target="_blank" rel="noreferrer"><img src="facebooklogo.png" alt="facebook" id="rss1"/></a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noreferrer"><img src="iglogo.png" alt="instagram" id="rss2" /></a>
+                    <a href="https://www.twitter.com" target="_blank" rel="noreferrer"><img src="twitterlogo.png" alt="twitter" id="rss3" /></a>
                 </div>
             </div>
         </StyledHeader>
